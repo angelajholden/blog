4c213603e9eb9511707054437235b57504b9fd0f
Angela J Holden <angelajholden@gmail.com>
2025-02-09 16:11:25 -0600

    footer styles, body styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const button = document.querySelector(".button_menu");
 	const menu = document.querySelector(".mobile_off_canvas");
+	const body = document.querySelector("body");
 
 	// when we click the button, add an active class to the button
 	// and add an active class to the menu
 	button.addEventListener("click", function () {
 		menu.classList.toggle("active");
 		button.classList.toggle("active");
+		body.classList.toggle("active");
 	});
 });
