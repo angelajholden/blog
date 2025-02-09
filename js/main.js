2f02236733aab7b06b4256d63dd40d1360e650ff
Angela J Holden <angelajholden@gmail.com>
2025-02-09 15:31:22 -0600

    mobile off canvas navigation styles

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const button = document.querySelector(".button_menu");
+	const menu = document.querySelector(".mobile_off_canvas");
+
+	// when we click the button, add an active class to the button
+	// and add an active class to the menu
+	button.addEventListener("click", function () {
+		menu.classList.toggle("active");
+		button.classList.toggle("active");
+	});
+});
